7a2fad1b440b6c55e8421e203841d9d96b1d00ea
Jimmy <miki3867@gmail.com>
2021-06-03 17:59:06 +0200

    It works

--- swapimage.py
+++ swapimage.py
@@ -1,12 +1,15 @@
 import os
 import random
+import sys
+
+path = sys.argv[1]
 
 def get_files(path: str) -> list:
     assert path is not None
 
     return [f for f in os.listdir(path)]
 
-def rename() -> None:
+def rename(path: str) -> None:
     img = random.choice(get_files('img/'))
 
     if img == "1.jpg":
@@ -15,4 +18,4 @@ def rename() -> None:
     os.rename("img/1.jpg", "img/" + str(random.randint(1, 99999999999))+ ".jpg")
     os.rename("img/" + img, "img/1.jpg")
 
-rename()
+rename(path)
